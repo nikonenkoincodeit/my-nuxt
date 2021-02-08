ebdaf606e257f6adfa4d71fcf17c130ebb182800
Yuriy <test@test.com>
2021-02-08 09:20:31 +0200

    --amend

---
+++ components/LastProducts.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div class="last-products">
+      <h2>Last added products</h2>
+
+      <section class="products">
+        <no-ssr>
+          <splide>
+            <splide-slide>
+              <img src="../assets/images/11.jpg" />
+            </splide-slide>
+            <splide-slide>
+              <img src="../assets/images/12.jpg" />
+            </splide-slide>
+            <splide-slide>
+              <img src="../assets/images/13.jpg" />
+            </splide-slide>
+          </splide>
+        </no-ssr>
+        <!-- <article>
+          <img src="../assets/images/11.jpg" alt="" />
+          <h3>Lorem ipsum dolor</h3>
+          <h4>$990.00</h4>
+          <a href="cart.html" class="btn-add">Add to cart</a>
+        </article>
+        <article>
+          <img src="../assets/images/12.jpg" alt="" />
+          <h3>cupidatat non proident</h3>
+          <h4>$1 200.00</h4>
+          <a href="cart.html" class="btn-add">Add to cart</a>
+        </article>
+        <article>
+          <img src="../assets/images/13.jpg" alt="" />
+          <h3>Duis aute irure</h3>
+          <h4>$2 650.00</h4>
+          <a href="cart.html" class="btn-add">Add to cart</a>
+        </article>
+        <article>
+          <img src="../assets/images/14.jpg" alt="" />
+          <h3>magna aliqua</h3>
+          <h4>$3 500.00</h4>
+          <a href="cart.html" class="btn-add">Add to cart</a>
+        </article> -->
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import NoSSR from "vue-no-ssr";
+export default {
+  name: "LastProducts",
+  components: {
+    "no-ssr": NoSSR
+  }
+};
+</script>
+
+<style lang="scss">
+@import "~/assets/css/splide-core.min.scss";
+@import "~/assets/css/splide.min.scss";
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,11 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["~/assets/css/styles.scss"],
+  css: [
+    "~/assets/css/styles.scss",
+    "~/assets/css/splide.min.scss",
+    "~/assets/css/splide-core.min.scss"
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
